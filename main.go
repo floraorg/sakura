f763e3deb5cff89402a1db1fa5cac87b159540a8
rexdotsh <65942753+rex69420@users.noreply.github.com>
2024-11-23 16:09:46 +0530

    feat: add port config to env

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	routes "github.com/floraorg/sakura/router"
 	"github.com/joho/godotenv"
@@ -12,6 +14,13 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := routes.SetupRouter()
-	r.Run()
+	fmt.Printf("Server starting on port :%s\n", port)
+	r.Run(":" + port)
 }
